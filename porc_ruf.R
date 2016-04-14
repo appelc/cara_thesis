5ceb2db696161d0d40364393638e15586f49ae22
Cara Appel <Cara Appel>
2016-04-13 23:30:19 -0700

    trying to write rasters

--- porc_ruf.R
+++ porc_ruf.R
@@ -13,10 +13,11 @@
 ##    b. For UD height at occurrece points only
 ## 5. Run RUF using package "ruf"
 
-library(googlesheets)
 library(adehabitatHR)
-library(ruf)
+library(googlesheets)
 library(raster)
+library(rgdal)
+library(ruf)
 
 ######################
 ## 1. First, load data
@@ -45,19 +46,13 @@ porc.sp <- SpatialPointsDataFrame(data.frame(porc.locs$utm_e, porc.locs$utm_n),
 ## 2. Then, extract the UD from "adehabitatHR" package
 ######################
 
-## Calculate KDE using kernelUD for all animals
+## Calculate KUD for all animals
 all.kud <- kernelUD(porc.sp, h="href")
 image(all.kud) #href doesn't look great
 
 ## revisit extent parameter (actually calculate for cell ~3-6 meters)
 all.kud <- kernelUD(porc.sp, h=60, extent=1, grid=1000)
 image(all.kud)
-points(porc.sp)
-
-# convert estUD to raster
-all.kud.raster <- raster(estUDm2spixdf(all.kud))
-###### ERROR: this function can only be used when the same grid was used for all animals
-###### (can add "same4all=TRUE" to kernelUD code, but I don't think that's what we want)
 
 ######################
 ## 3. Then, create a table with id, coord, and UD height
@@ -66,22 +61,55 @@ all.kud.raster <- raster(estUDm2spixdf(all.kud))
 ######################
 
 ids <- names(all.kud)
-porc_uds <- NULL
+#porc_uds <- NULL
 
 for(i in ids){
       ud.height.i <- all.kud[[i]]@data$ud
       coords.i <- all.kud[[i]]@coords
       ht.i <- cbind((rep(i, length(ud.height.i))), ud.height.i, coords.i)
       colnames(ht.i) <- c("id", "height", "x", "y")
-      porc_uds <- rbind(porc_uds, ht.i)
+     # porc_uds <- rbind(porc_uds, ht.i)
+      mypath <- file.path("C:","Users","Cara","Documents", "cara_thesis", "RUFs",
+                           paste(i, "_ud", ".csv", sep = ""))
+      write.csv(ht.i, file=mypath)
 }
+
 write.csv(porc_uds, "porc_uds.csv")
+
 ## too big... do them separately!
 
 ######################
 ## b. Get UD height at occurrence points only
 ######################
 
-# or you can extract just at the coordinates from the raster
+## first, convert estUD to raster
+## need to do separately for each animal (I get an error in "raster" function that it only works
+## when all animals have the same grid... can add "same4all=TRUE", but is this what we want?)
+## NOt as easy to just do "raster" with one animal, because using something like "kud.all[1]" makes
+## it not an "estUDm" object anymore (estUD-multiple animals) but an "estUD" (single animal), and
+## the "estUDm2spixdf" will only convert an estUDm object to a SPDF, not an estUD object... phew.
+
+## this should be a loop, but I can't figure out how to save the rasters separately
+## could do writeOGR instead of raster function?
+## otherwise, have to do this each time?
+
+#ids <- names(all.kud)
+i <- ids[17] # just change number here each time
+#for(i in ids){
+i.locs <- subset(porc.locs, id == i)
+i.locs <- droplevels(i.locs)
+i.sp <- SpatialPointsDataFrame(data.frame(i.locs$utm_e, i.locs$utm_n),
+                               data=data.frame(i.locs$id),
+                               proj4string=CRS("+proj=utm +zone=10 +datum=NAD83"))
+i.kud <- kernelUD(i.sp, h=60, extent=1, grid=1000)
+i.raster <- raster(estUDm2spixdf(i.kud))
+#}
+
+## and rename it here each time
+raster18 <- i.raster
+plot(raster18)
+
+# now, you can extract just at the coordinates from the raster
 porc.sp$udheight <- extract(all.kud.raster, porc.sp)
+test.udheight <- extract(raster01, porc.sp)
 
