5d741e8832ca041f326b125344cfffa288632cfa
Your Name <cla236@humboldt.edu>
2016-05-02 20:07:17 -0700

    added code to create 99% contour for all porcs (as extent), subsample random points within it, and overlay veg

--- porc_glmm.R
+++ porc_glmm.R
@@ -1,18 +1,123 @@
 #############################################################
 ### Taking a stab at fitting a GLMM in WinBugs with porc data
 ### based on notes from STAT 510 lab (Mar 28)
-#############################################################
-
-library(R2WinBUGS)
-bugs.dir <- "C:/Programs/WinBugs/winbugs14"
 
 ## the lab does have code for simulating binomial data
 ## (would need to do multinomial, actually)
+#############################################################
 
+library(googlesheets)
+library(rgdal)
+library(raster)
+library(rgeos)
+library(R2WinBUGS)
+bugs.dir <- "C:/Programs/WinBUGS14"
 
+## import data
+gs_ls()
+locs <- gs_title("Porc relocation data")
+porc.locs <- data.frame(gs_read(ss=locs, ws="Relocations", is.na(TRUE), range=cell_cols(1:8)))
+colnames(porc.locs) <- c("date", "id", "sess", "type", "time", "az", "utm_e", "utm_n")
+porc.locs <- subset(porc.locs, type %in% c("V","V*","P","P*","L"))
+porc.locs$utm_e <- as.numeric(porc.locs$utm_e)
+porc.locs$utm_n <- as.numeric(porc.locs$utm_n)
+## check date format before running line 51 or 52
+#porc.locs$date <- as.Date(porc.locs$date, "%m/%d/%Y") 
+#porc.locs$date <- as.Date(porc.locs$date, origin = as.Date("1899-12-30"))
 
+## make a column to say that these are all collared porc locations
+## (we're not going to use individual ID)
+porc.locs$name <- rep("collared_porcs", nrow(porc.locs))
 
+## make SPDF for all points on collared animals (not distinguished by individual)
+all.loc.sp <- SpatialPointsDataFrame(data.frame(porc.locs$utm_e, porc.locs$utm_n),
+                                  data=data.frame(porc.locs$name),
+                                  proj4string=CRS("+proj=utm +zone=10 +datum=NAD83"))
 
+## Now make a subset of individual porcupine locations during summer only (before Nov 1)
+sum.locs <- subset(porc.locs, date < "2015-11-01")
+sum.sp <- SpatialPointsDataFrame(data.frame(sum.locs$utm_e, sum.locs$utm_n),
+                                 data=data.frame(sum.locs$id),
+                                 proj4string = CRS("+proj=utm +zone=10 +datum=NAD83"))
+
+## Load veg data
+veg <- readOGR(dsn="shapefiles", layer="Veg categories CA", verbose=TRUE)
+proj4string(veg) <- proj4string(all.loc.sp)
+
+## I also have a dissolved version of the veg shapefile to use for clipping
+veg_ext <- readOGR(dsn="shapefiles", layer="Veg extent", verbose=TRUE)
+proj4string(veg_ext) <- proj4string(all.loc.sp)
+
+## Use 99% KDE for all animals as the extent for selecting "available" points
+## first, calculate grid size and estimate kernelUD
+c = 10   ## desired cell size (meters)
+fake.kern <- kernelUD(xy = all.loc.sp, extent = 1)
+spdf <- raster(as(fake.kern[[1]], "SpatialPixelsDataFrame"))
+eas <- diff(range(spdf@extent[1:2]))
+nor <- diff(range(spdf@extent[3:4]))
+if(eas > nor){
+  g <- (eas/c)
+} else {
+  g <- (nor/c)
+}
+
+## Do 99% KDE on ALL points, not just summer (is this right?)
+kernel.all <- kernelUD(xy = all.loc.sp, h = 60, grid = g, extent = 1)
+image(kernel.all) #how do they look?
+cont99.all <- getverticeshr.estUD(kernel.all[[1]], percent = 99, unin = "m", unout = "km2", standardize = FALSE)
+
+## and clip to "veg" extent
+cont99.clipped <- gIntersection(veg_ext, cont99.all, byid = FALSE, drop_lower_td = TRUE)
+
+## so "cont99.clipped" is the extent within which we will sample "available" points
+
+## also clip "sum.sp" to veg_extent (there are a few outside
+sum.sp.clipped <- sum.sp[veg_ext,]
+
+## Create random points within the available study area
+## use "nrow()" to make sure we have the same no. of avail. pts as used pts
+## we're using summer points only now
+sum.avail.pts <- spsample(cont99.clipped, n=(nrow(sum.sp.clipped)), type="random")
+
+## plot everything:
+plot(veg_ext) # need to fix annoying holes...
+plot(cont99.clipped, add=TRUE, col="green")
+plot(sum.sp.clipped, add=TRUE, pch=16, cex=0.4)
+plot(sum.avail.pts, add=TRUE, col="red", pch=16, cex=0.4)
+
+## Now add 0/1 for pres/avail
+## and add columns for "season"
+sum.pts <- data.frame(sum.sp.clipped$sum.locs.id, sum.sp.clipped@coords) # convert to data.frame
+sum.pts$pres <- rep(1, nrow(sum.pts)) # add a 1 for all presence (used) pts
+sum.pts$pres_name <- rep("present", nrow(sum.pts)) # add a column for description
+colnames(sum.pts) <- c("id", "x", "y", "pres", "pres_name")
+
+## Do the same for the "available" points we created
+avail.pts <- data.frame(sum.avail.pts@coords)
+avail.pts$pres <- rep(0, nrow(avail.pts)) # add a 0 for all available pts
+avail.pts$pres_name <- rep("available", nrow(avail.pts))
+avail.pts$id <- rep("avail", nrow(avail.pts))
+avail.pts <- avail.pts[,c("id", "x", "y", "pres", "pres_name")] # rearrange to match "sum.pts"
+
+## combine used and available pts
+sum.used.avail <- rbind(sum.pts, avail.pts)
+nrow(sum.used.avail) # good, it should be the number of summer points (clipped) * 2
+
+## now overlay "veg"
+## need to make SPDF again... should have done this before?
+points.sp <- SpatialPointsDataFrame(data.frame(sum.used.avail$x, sum.used.avail$y),
+                                            data = data.frame(sum.used.avail$id, sum.used.avail$pres),
+                                            proj4string = CRS(proj4string(all.loc.sp)))
+
+points.sp$veg <- over(points.sp, veg)$Class_2
+points.sp <- points.sp[!is.na(points.sp$veg),] # get rid of veg=NA
+
+points.df <- data.frame(points.sp@data)
+colnames(points.df) <- c("id", "pres", "veg")
+test <- aggregate(points.df, FUN = "sum", by = unique(veg$Class_2)) ## fix
+View(test, FUN="sum")
+
+###############
 
 sink("BinomGLM1.txt")
 cat("
