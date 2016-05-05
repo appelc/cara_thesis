fc809e60df918ec4238dc23a4f253c70622db301
Your Name <cla236@humboldt.edu>
2016-05-05 15:33:42 -0700

    made line plot in ggplot and ran t-test again

--- season-weight.R
+++ season-weight.R
@@ -9,14 +9,11 @@ library(googlesheets)
 library(ggplot2)
 library(dplyr)
 
-setwd("C:/Users/Cara/Documents/__RESEARCH/ANALYSIS/033016")
-
 ## load from Google Drive
 gs_ls()
 wts <- gs_title("Porc weights & captures")
-wts
 porc.wts <- data.frame(gs_read(ss=wts, ws="Weights", is.na(TRUE), range=cell_cols(1:5)))
-colnames(porc.wts) <- c("id", "date", "wt", "sex", "month")
+colnames(porc.wts) <- c("id", "date", "kg", "sex", "month")
 head(porc.wts)
 
 ## format correctly
@@ -24,21 +21,45 @@ porc.wts$id <- as.factor(porc.wts$id)
 porc.wts$date <- as.Date(porc.wts$date, "%m/%d/%Y")
 porc.wts$sex <- as.factor(porc.wts$sex)
 porc.wts$month <- as.factor(porc.wts$month)
+porc.wts$kg <- as.numeric(porc.wts$kg)
 
 ## subset males/females
-m.wts <- subset(porc.wts, sex == "M")
+m.wts <- porc.wts[porc.wts$sex == "M",]
 m.wts <- droplevels(m.wts)
-f.wts <- subset(porc.wts, sex == "F")
+f.wts <- porc.wts[porc.wts$sex == "F",]
 f.wts <- droplevels(f.wts)
+
+## get rid of singletons (Bowie and Badger)?
+f.wts <- f.wts[f.wts$id != "15.05" & f.wts$id != "16.16",]
 
 ##########################################################
 ### (1) create figure with trends for each animal
 ##########################################################
 
-ggplot(data=porc.wts, aes(x=date, y=wt, group=id)) +
-          geom_point(colour = 'black', size = 3) + geom_line(colour = 'black', size = 1) +
-          geom_point(data = m.wts, aes(y = m.wts$wt),
-             colour = 'grey', size = 3) + geom_line(data=m.wts, colour = 'grey', size = 1)
+ggplot(data=porc.wts, aes(x = date, y = wt, group = id)) +
+          geom_point(data = f.wts, aes(y = f.wts$wt, colour = 'Female', shape = 'Female'), size = 5) + 
+          geom_line(data = f.wts, colour = 'black', size = 2) +
+          geom_point(data = m.wts, aes(y = m.wts$wt, colour = 'Male', shape = 'Male'), size = 5) + 
+          geom_line(data = m.wts, colour = 'darkgray', size = 2, lty = 2) +
+          ylab('Body mass (kg)') + xlab('Date') + 
+          scale_y_continuous(limits = c(4,11)) +
+          theme(axis.text=element_text(size=20, colour = 'black'),
+                axis.title=element_text(size=24, colour = 'black'),
+                axis.line.x = element_line(size = 1, colour = 'black'),
+                axis.line.y = element_line(size = 1, colour = 'black'),
+                axis.ticks = element_line(size = 1, colour = 'black'),
+                axis.ticks.length = unit(.3, 'cm'),
+                panel.background = element_rect(fill = 'white'),
+                legend.background = element_rect(colour = 'white'),
+                legend.key = element_rect(fill = 'white'),
+                legend.text = element_text(size = 20),
+                legend.key.size = unit(1.5, "cm")) +     
+          scale_colour_manual("", 
+                      breaks = c('Female', 'Male'),
+                      values = c('black', 'darkgray')) +
+          scale_shape_manual("",
+                      breaks = c('Female', 'Male'),
+                      values = c(15, 16))
 
 ##########################################################
 ### (2) linear model to find informed seasonal cutoff
@@ -62,21 +83,21 @@ abline(lm1)
 ## (maybe I could actually keep them all until the end...)
 porc_2015 <- grepl('^15', porc.wts$id) ## ask which ones start with "15" 
 porc_2015 # (TRUE/FALSE)
-porc.wts <- porc.wts[porc_2015,] ## then only keep the rows with TRUE
-porc.wts <- droplevels(porc.wts)
+porc.wts15 <- porc.wts[porc_2015,] ## then only keep the rows with TRUE
+porc.wts15 <- droplevels(porc.wts15)
 
 ## subset m/f again for animals captured in 2015
-f.wts15 <- subset(porc.wts, sex="F")
+m.wts15 <- porc.wts15[porc.wts15$sex == "M",]
+m.wts15 <- droplevels(m.wts15)
+f.wts15 <- porc.wts15[porc.wts15$sex == "F",]
 f.wts15 <- droplevels(f.wts15)
-m.wts15 <- subset(porc.wts, sex="M")
-m.wts15 <- droplevels(f.wts15)
 
 ##### FROM HERE FORWARD, NEED TO SELECT A CUTOFF DATE
 
 ## separate summer/winter weights
 ## could do this all iteratively again, I suppose
-s.wts <- subset(porc.wts, date < as.Date("2015-10-01"))
-w.wts <- subset(porc.wts, date >= as.Date("2015-10-01"))
+s.wts <- porc.wts15[porc.wts15$date < as.Date("2015-11-01"),]
+w.wts <- porc.wts15[porc.wts15$date >= as.Date("2015-11-01"),]
 
 ## if multiple summer weights, need to take the average
 ## could use "table" to ask which ones have >1 summer weight (it's only 15.12), 
@@ -86,13 +107,13 @@ ids <- unique(s.wts$id)
 s.avgs <- NULL
 
 for(i in ids){
-  s.avg.i <- mean(s.wts$wt[(which(s.wts$id == i))])
-  sex.i <- s.wts$sex[which(s.wts$id == i)]
+  s.avg.i <- mean(s.wts$kg[s.wts$id == i])
+  sex.i <- s.wts$sex[s.wts$id == i]
   wt.id <- data.frame(i, s.avg.i, sex.i)
+  colnames(wt.id) <- c("id", "s_avg", "sex")
   s.avgs <- rbind(s.avgs, wt.id)
 }
 
-colnames(s.avgs) <- c("id", "s_wt", "sex")
 dups <- duplicated(s.avgs[, ("id")])
 s.avgs <- s.avgs[!dups,]
 
@@ -103,13 +124,13 @@ ids <- unique(w.wts$id)
 w.avgs <- NULL
 
 for(i in ids){
-    w.avg.i <- mean(w.wts$wt[(which(w.wts$id == i))])
+    w.avg.i <- mean(w.wts$kg[w.wts$id == i])
     wt.id <- data.frame(i, w.avg.i)
+    colnames(wt.id) <- c("id", "w_avg")
     w.avgs <- rbind(w.avgs, wt.id)
 }
 
-colnames(w.avgs) <- c("id", "w_wt")
-w.avgs$w_wt <- round(w.avgs$w_wt, 2)
+w.avgs$w_avg <- round(w.avgs$w_avg, 3)
 
 ## now combine them 
 ## hmm, maybe I could even leave in ones that only have winter until this point
@@ -117,17 +138,17 @@ avg.wts <- left_join(s.avgs, w.avgs) ## will get a warning; see next step
 avg.wts$id <- as.factor(avg.wts$id) 
 
 ## now get rid of rows with NA
-avg.wts <- avg.wts[!is.na(avg.wts$w_wt),]
-avg.wts <- avg.wts[,c("id", "sex", "s_wt", "w_wt")]
+avg.wts <- avg.wts[!is.na(avg.wts$w_avg),]
+avg.wts <- avg.wts[,c("id", "sex", "s_avg", "w_avg")]
 avg.wts
 
 ## now do statistics!
-t.test(avg.wts$s_wt, avg.wts$w_wt, paired=TRUE)
+t.test(avg.wts$s_avg, avg.wts$w_avg, paired=TRUE)
 
-mean_s <- mean(avg.wts$s_wt)
-mean_s # 8.497 kg
-mean_w <- mean(avg.wts$w_wt)
-mean_w # 7.79 kg
+mean_s <- mean(avg.wts$s_avg)
+mean_s # 8.497 kg ## why do I get a different answer now? 8.192 kg (05/05/16)
+mean_w <- mean(avg.wts$w_avg)
+mean_w # 7.79 kg ## 7.403 kg (05/05/16)
 means <- cbind(mean_s, mean_w)
 
 par(mfrow=c(1,1))
